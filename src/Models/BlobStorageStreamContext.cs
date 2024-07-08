12f8a55ba11dd332442c36110e04db7dc22f5e2e
Vitalii Savitskii <visa@ecco.com>
2024-07-08 13:56:34 +0200

    Exclude context from coverage

--- src/Models/BlobStorageStreamContext.cs
+++ src/Models/BlobStorageStreamContext.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using Arcane.Framework.Services.Base;
 
 namespace Arcane.Stream.BlobStorage.Models;
 
+[ExcludeFromCodeCoverage(Justification = "Model")]
 public class BlobStorageStreamContext : IStreamContext, IStreamContextWriter
 {
     /// <summary>

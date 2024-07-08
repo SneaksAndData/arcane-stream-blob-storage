d7ec083241b3931cd6ca7c9dad3958e7b3947d6c
Vitalii Savitskii <visa@ecco.com>
2024-07-08 14:15:57 +0200

    Add blob storage stream implementation (#11)
    * Add blob storage stream implementation
    
    * Add unit tests and complete the implementation.
    
    * Exclude context from coverage

---
+++ src/Exceptions/SinkException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Arcane.Stream.BlobStorage.Exceptions;
+
+/// <summary>
+/// An exception thrown when a sink operation fails
+/// </summary>
+public class SinkException : Exception
+{
+    public SinkException(string message): base(message)
+    {
+    }
+}
---
+++ src/Extensions/StreamContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Akka.Streams.Dsl;
+using Arcane.Stream.BlobStorage.Exceptions;
+using Arcane.Stream.BlobStorage.Models;
+using Snd.Sdk.Storage.Base;
+using Snd.Sdk.Storage.Models.Base;
+
+namespace Arcane.Stream.BlobStorage.Extensions;
+
+public static class StreamContextExtensions
+{
+    public static Sink<(IStoragePath, string), Task> GetSink(this BlobStorageStreamContext context, IBlobStorageService blobStorageService)
+    {
+        return Sink.ForEachAsync<(IStoragePath, string)>(context.DeleteParallelism, async deleteRequest =>
+        {
+            var (sourceRoot, sourceBlobName) = deleteRequest;
+            var res = await blobStorageService.RemoveBlob(sourceRoot.ObjectKey, sourceBlobName);
+            if (!res)
+            {
+                throw new SinkException($"Failed to remove blob {sourceBlobName} from {sourceRoot}");
+            }
+        }).MapMaterializedValue(v => (Task)v);
+    }
+}

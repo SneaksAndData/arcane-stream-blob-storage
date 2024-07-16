06d88a606861eac4acd55ae61f6a597b9620f64a
Vitalii Savitskii <visa@ecco.com>
2024-07-16 16:36:30 +0200

    Implement metrics collection for specific to the Blob Storage Stream
    Resolves #27

---
+++ src/Contracts/DeclaredMetrics.cs
@@ -0,0 +1,19 @@
+﻿namespace Arcane.Stream.BlobStorage.Contracts;
+
+/// <summary>
+/// Metrics specific to the BlobStorage stream plugin.
+/// </summary>
+public class DeclaredMetrics
+{
+    /// <summary>Number of objects found by streamer in the source bucket.</summary>
+    public const string OBJECTS_INCOMING = "objects.incoming";
+    
+    /// <summary>Number of objects written by streamer into the target bucket.</summary>
+    public const string OBJECTS_OUTGOING = "objects.outgoing";
+    
+    /// <summary>Number objects size processed by the streamer.</summary>
+    public const string OBJECTS_SIZE = "objects.size";
+    
+    /// <summary>Number of objects deleted by the streamer in the source bucket.</summary>
+    public const string OBJECTS_DELETED = "objects.size";
+}
---
+++ src/Extensions/AmazonS3StoragepathExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Snd.Sdk.Storage.Models.BlobPath;
+
+namespace Arcane.Stream.BlobStorage.Extensions;
+
+public static class AmazonS3StoragePathExtensions
+{
+    /// <summary>
+    /// Converts the Amazon S3 storage path to a dictionary of metrics tags
+    /// </summary>
+    /// <param name="path">Path</param>
+    /// <returns>Metrics tags</returns>
+   public static SortedDictionary<string, string> ToMetricsTags(this AmazonS3StoragePath path)
+   {
+       return new SortedDictionary<string, string>
+       {
+           { "bucket", path.Bucket },
+           { "key", path.ObjectKey }
+       };
+   }
+}
